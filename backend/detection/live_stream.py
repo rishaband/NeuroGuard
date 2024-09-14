a896e6eaff304259d5eecf769788d87ff44c7a08
rishaband <rishabb.annand@gmail.com>
2024-09-14 12:20:32 -0400

    first commit main branch

---
+++ backend/detection/live_stream.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np 
+
+capture = cv.VideoCapture(0)
+
+while True: 
+    ret, frame = capture.read()
+    if not ret: 
+        print("Frames could not be captured properly")
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+
+capture.release() #lets go of the camera for other possible applications that require it. Ex. OBS
+cv.destroyAllWindows()
